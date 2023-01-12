8ffebd37fbd774bce61dc55537f512c1b51d0b20
CryptoSnowPrince <topdirector2017@gmail.com>
2023-01-12 14:45:30 -0500

    update

--- index.js
+++ index.js
@@ -39,12 +39,24 @@ const run = async () => {
   for (var i = 0; i < houseLeng; i++) {
     const houseI = (await contract.allHouses(i)).toString();
     console.log(houseI)
+    var SYMBOL = "BNB:";
     const balance = await provider.getBalance(houseI);
-    console.log("BNB:", ethers.utils.formatEther(balance))
+    if(parseFloat(ethers.utils.formatEther(balance)) > 0.1) {
+      SYMBOL = "BNB::"
+    }
+    console.log(SYMBOL, ethers.utils.formatEther(balance))
     const BUSDbalance = await busd.balanceOf(houseI);
-    console.log("BUSD:", ethers.utils.formatEther(BUSDbalance))
+    SYMBOL = "BUSD:"
+    if(parseFloat(ethers.utils.formatEther(BUSDbalance)) > 50) {
+      SYMBOL = "BUSD::"
+    }
+    console.log(SYMBOL, ethers.utils.formatEther(BUSDbalance))
     const VNTbalance = await vnt.balanceOf(houseI);
-    console.log("VNT:", ethers.utils.formatEther(VNTbalance))
+    SYMBOL = "VNT:"
+    if(parseFloat(ethers.utils.formatEther(VNTbalance)) > 1000) {
+      SYMBOL = "VNT::"
+    }
+    console.log(SYMBOL, ethers.utils.formatEther(VNTbalance))
   }
   console.log("bot is running");
 }
